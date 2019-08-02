a47a916dd9837f915d566802a429319c77baa6d3
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 19:27:22 +0300

    Some changes

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string task (unsigned  int a)
 {
-   if (a>1000)
+    if (a>1000)
     {
         if (a>1000000)return  "Ti millioner!";
         return "Horosho";
@@ -11,11 +11,11 @@ string task (unsigned  int a)
     return "Rabotai bolshe";
 }
 
-   int main()
-   {
+int main()
+{
     unsigned  int a ;
     cout<<"Vasha zarplata($): ";
     cin >>a;
     cout <<task(a)<<"...no ti molodec";
     return 0;
-   }
+}
